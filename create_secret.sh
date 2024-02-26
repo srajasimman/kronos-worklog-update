92a2335c6b2a09faf82f48d0afda89b9a8b2adee
Rajasimman S <srajasimman@gmail.com>
2024-02-26 10:19:12 +0530

    🆙 update

--- create_secret.sh
+++ create_secret.sh
@@ -3,6 +3,9 @@
 # write a bash script to read user input for the credentials KRONOS_EMAIL and KRONOS_password
 # and create a kube secret for the cronjob
 
+# read KRONOS_DOMAIN
+read -p "Enter KRONOS_DOMAIN: " KRONOS_DOMAIN
+
 # read KRONOS_EMAIL
 read -p "Enter KRONOS_EMAIL: " KRONOS_EMAIL
 
@@ -10,7 +13,10 @@ read -p "Enter KRONOS_EMAIL: " KRONOS_EMAIL
 read -s -p "Enter KRONOS_PASSWORD: " KRONOS_PASSWORD
 
 kubectl create secret generic kronos-secret \
+    --from-literal=KRONOS_DOMAIN="$KRONOS_DOMAIN" \
     --from-literal=KRONOS_EMAIL="$KRONOS_EMAIL" \
     --from-literal=KRONOS_PASSWORD="$KRONOS_PASSWORD" \
     --namespace default -o yaml \
-    --dry-run=client > kube_secret.yaml
+    --dry-run=client | kubectl apply -f -
+
+kubectl apply -f kube_cronjob.yaml
